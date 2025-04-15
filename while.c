6b682dc71c09e7d44a6e795217810a4c0445bcb9
bintangnaufal21 <105490378+bintangnaufal21@users.noreply.github.com>
2025-04-15 16:33:11 +0700

    upload

---
+++ while.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main(){
+
+    char ulangi = 'y';
+    int counter = 0;
+
+    //!perulangan while
+    while (ulangi == 'y')
+    {
+        printf("Apakah kamu mau mengulang");
+        printf("Jawab (y/N)");
+        scanf("%c", &ulangi);
+        
+        //!increment counter
+        counter++;
+    }
+
+    printf("\n\n-------\n");
+    printf("Perulangan selesai\n");
+    printf("Kamu mengulang sebanyak %i kali.\n", counter);
+}
